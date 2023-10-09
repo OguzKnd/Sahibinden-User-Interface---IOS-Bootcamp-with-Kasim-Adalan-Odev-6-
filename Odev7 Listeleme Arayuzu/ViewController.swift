296f1ca85cdf9974b9c52ef41e208d26deddb445
Oguz Kanda <exporler95@gmail.com>
2023-10-09 16:56:50 +0300

    Initial Commit

---
+++ Odev7 Listeleme Arayuzu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev7 Listeleme Arayuzu
+//
+//  Created by Oğuz Kanda on 9.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
